52b26fbef301e9bf9bf4ec8ff731a95d9eae57f3
James Marrs <jamesmarrs@JM-personal-laptop.local>
2021-07-28 18:37:27 +0100

    third draft

--- copter/tests/combined/scripts/app.js
+++ copter/tests/combined/scripts/app.js
@@ -11,7 +11,7 @@ img.src = 'assets/heli.png';
 topBlockContext.fillStyle = 'yellow'
 let one = 1
 let heliXInitial = 200
-let initialBlockHeights = 50
+let initialBlockHeights = 20
 let score = 0
 let hundred = 0
 let level = 1
@@ -43,23 +43,36 @@ function makeMountainArrays() {
 topMountainArray = []
 bottomMountainArray = []
 
-////////TOP PART////////////
-//pre entering cave 400 parts
+//pre entering cave 400 parts///////
+//top
 let topBlockY = 0
+let bottomBlockY = 0
 for (let i = 0; i<= 399; i++) {
     topMountainArray.push(0)
-}
+    bottomMountainArray.push(0)
+  }  
 
-/// slope into cave
+/// slope into cave///////////
+//top
 for (let i = 400; i<= 400+topBlockLimits.height-1; i++) {
     topMountainArray.push(i-400)
 }
 
-//cave enter
+//bottom
+for (let i = 400; i<= 400+bottomBlockLimits.height-1; i++) {
+  bottomMountainArray.push(i-400)
+}
+
+//cave enter////////////////
+//top
 topBlockY = topBlockLimits.height
+let topCounter = 0
 for (let i = 500; i<= 99999; i++) {
+  if (topCounter % 1000 === 0 && topCounter > 0){
+    topBlockLimits.height +=20
+  }
   topMountainArray.push(topBlockY) 
-  randNo = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+  randNo = Math.floor(Math.random() * (30 - 1 + 1)) + 1;
   if (randNo === 5){
     one = -one;
   } 
@@ -70,25 +83,19 @@ for (let i = 500; i<= 99999; i++) {
   one = -one;
 }
 topBlockY = topBlockY + one;
+topCounter++
 }
 
-////////BOTTOM PART////////////
-//pre entering cave 400 parts
-let bottomBlockY = 0
-for (let i = 0; i<= 399; i++) {
-  bottomMountainArray.push(0)
-}
 
-/// slope into cave
-for (let i = 400; i<= 400+bottomBlockLimits.height-1; i++) {
-  bottomMountainArray.push(i-400)
-}
-
-//cave enter
+//bottom part
 bottomBlockY = bottomBlockLimits.height
+let bottomCounter = 0
 for (let i = 500; i<= 99999; i++) {
+  if (bottomCounter % 1000 === 0 && bottomCounter > 0){
+    bottomBlockLimits.height +=20
+  }
   bottomMountainArray.push(bottomBlockY) 
-  randNo = Math.floor(Math.random() * (11 - 1 + 1)) + 1;
+  randNo = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
   if (randNo === 5){
     one = -one;
   } 
@@ -99,15 +106,19 @@ for (let i = 500; i<= 99999; i++) {
   one = -one;
 }
 bottomBlockY = bottomBlockY + one;
-}
+bottomCounter++
 }
 
+}
 
 makeMountainArrays()
-let gamePosition = 400
+let gamePosition = 0
 
-//heli and background movement
-setInterval(() => {
+
+
+//background movement
+let gameContinue = true;
+const handlegame = setInterval(() => {
   topBlockContext.clearRect(0, 0, 400, 300)
   for (let i = 0; i<= 400; i++) {
     topBlockContext.fillRect(i, 0, 1, topMountainArray[gamePosition + i])
@@ -115,54 +126,73 @@ setInterval(() => {
    for (let i = 0; i<= 400; i++) {
     bottomBlockContext.fillRect(i, 300 - bottomMountainArray[gamePosition + i], 1, bottomMountainArray[gamePosition + i])
    }
+   heliMovement()
+   function heliMovement() {
+   heliContext.clearRect(0, 0, 400, 300)
+   heliContext.drawImage(img, helicopter.xPosition, helicopter.yPosition, 100, 33);
+     //helicopter.xPosition += xMove
+     helicopter.yPosition += yMove
+   
+     //heli collision
+     for (let i = 0; i<= helicopter.width; i++) {
+     if (helicopter.yPosition <= topMountainArray[gamePosition+heliXInitial+i]) {
+     //  console.log('top collisio')
+     background.classList.add('collision')
+    gameContinue = false;
+     setTimeout(reset, 2000);
+     yMove = 0
+//     clearInterval(handlegame)     
+     }  
+     else if (helicopter.yPosition + helicopter.height >= 300 - bottomMountainArray[gamePosition + heliXInitial + i]) {
+     //  console.log('bottom collision')
+       background.classList.add('collision') 
+      gameContinue = false;
+       setTimeout(reset, 2000);
+       yMove = 0
+//       clearInterval(handlegame)
+     }
+     else {
+      //   console.log('safe')
+         background.classList.remove('collision')    
+     }
+     }
+    }
 
-heliContext.clearRect(0, 0, 400, 300)
-heliContext.drawImage(img, helicopter.xPosition, helicopter.yPosition, 100, 33);
-  //helicopter.xPosition += xMove
-  helicopter.yPosition += yMove
-
-  //heli collision
-  for (let i = 0; i<= helicopter.width; i++) {
-  if (helicopter.yPosition <= topMountainArray[gamePosition+heliXInitial+i]) {
-  //  console.log('top collisio')
-    background.classList.add('collision')
-  }  
-  else if (helicopter.yPosition + helicopter.height >= 300 - bottomMountainArray[gamePosition + heliXInitial + i]) {
-  //  console.log('bottom collision')
-    background.classList.add('collision')    
-  }
-  else {
-   //   console.log('safe')
-      background.classList.remove('collision')    
-  }
-  }
-  
+    if (gameContinue === true){
  gamePosition++
+}
 
+ if (gamePosition >= 200) {
+ if (gamePosition % 10 === 0) {
+  score++
+  currentScore.textContent = score
+  if (score % 100 === 0) {
+    level++
+    currentLevel.textContent = level
+   }
+ }
+
+}
+ 
   }, 10)
 
 
-  setInterval(() => {
-    if (hundred === 100){
-      hundred = 0
-      level ++
-    }
-    score++
-    hundred++
-    currentScore.textContent = score
-
-    currentScore.textContent = score
-    currentLevel.textContent = level
-  }, 100)
 
 function onkeydown(e) {
     //up key
     if (e.keyCode === 38) {
-         helicopter.yPosition -= 30;
-    }
+      yMove = -2
+    } 
     if (e.keyCode === 40) {
-      helicopter.yPosition += 30;
+      yMove = helicopter.yMoveInitial + 1 
  }
+  }
+
+  function onkeyup(e) {
+    //up key
+    if (e.keyCode === 38) {
+      yMove = helicopter.yMoveInitial
+    }
   }
 
   function reset() {
@@ -173,6 +203,10 @@ function onkeydown(e) {
       gamePosition = 0
       score = 0
       level = 1
+      gameContinue = true
+      currentLevel.textContent = level
+      currentScore.textContent = score
     }
   window.addEventListener("keydown", onkeydown);
+  window.addEventListener("keyup", onkeyup);
   heliCanvas.addEventListener('click', reset)
